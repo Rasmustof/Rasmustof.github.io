664014fe4186f856b5cb4a1201bf0165b9565ff7
Rasmus Toftegaard <rallen.l.t@gmail.com>
2019-10-24 09:58:23 +0200

    Updates

---
+++ precache-manifest.992f8ed306e472be4f32268d00e6ba4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24180091c20757ac7e174e87b43e9fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac492cef5562d9093124",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "bade69247da324b3a7a6",
+    "url": "/static/css/main.6ed4788d.chunk.css"
+  },
+  {
+    "revision": "ac492cef5562d9093124",
+    "url": "/static/js/2.8afebfb1.chunk.js"
+  },
+  {
+    "revision": "bade69247da324b3a7a6",
+    "url": "/static/js/main.94c00ad9.chunk.js"
+  },
+  {
+    "revision": "d9091079ad72bb325bd9",
+    "url": "/static/js/runtime-main.493472ac.js"
+  }
+]);
